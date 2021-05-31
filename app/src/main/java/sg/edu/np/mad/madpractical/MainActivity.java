cd6d735e97b4a0095fb489c671c91a4f82931514
julianee-jk <s10202587@connect.np.edu.sg>
2021-06-01 03:44:53 +0800

    pls work

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        db.deleteDB(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        User userInfo = new User(
